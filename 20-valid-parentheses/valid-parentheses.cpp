957966c426aebd85da8fa0d7150ffe9686a6bd99
PREM MUTHA <premmutha31@gmail.com>
2026-08-13 20:02:59 +0530

    Time: 2 ms (14.85%) | Memory: 8.7 MB (89.72%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
+
         for(int i = 0 ; i < s.length() ; i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+            if(s[i] == '[' || s[i] == '{' || s[i] == '('){
                 st.push(s[i]);
             }
             else{
-                if(st.empty()){
+                if(st.size() == 0){
                     return false;
                 }
-                if((st.top() == '(' && s[i] == ')')
-                || (st.top() == '{' && s[i] == '}')
-                || (st.top() == '[' && s[i] == ']')){
+                if((st.top() == '(' && s[i] == ')') || 
+                (st.top() == '[' && s[i] == ']') || 
+                (st.top() == '{' && s[i] == '}')){
                     st.pop();
                 }
                 else{
@@ -20,6 +21,6 @@ public:
                 }
             }
         }
-        return st.empty();
+        return st.size() == 0;
     }
 };
