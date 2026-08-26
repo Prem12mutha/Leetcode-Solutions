606778a2b18df8d93962d55aa5732b0bd7793ada
PREM MUTHA <premmutha31@gmail.com>
2026-08-26 23:38:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.61%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev = 0; // use long long temporarily
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+            // check overflow immediately
+            if (rev > INT_MAX || rev < INT_MIN) return 0;
+        }
+        return (int)rev;
+    }
+};
