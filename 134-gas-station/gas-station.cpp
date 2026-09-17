796437567c69e5c43d9ee6e286c591667ba10f9d
PREM MUTHA <premmutha31@gmail.com>
2026-09-17 19:58:54 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (32.53%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -3,22 +3,13 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int totGas = 0 , totCost = 0;
 
-        for(int val : gas){
-            totGas += val;
-        }
-
-        for(int val : cost){
-            totCost += val;
-        }
-
-        if(totGas < totCost){
-            return -1;
-        }
-
         // unique sol exits
         int start = 0 , currGas = 0;
 
         for(int i = 0 ; i < gas.size() ; i++){
+            totGas += gas[i];
+            totCost += cost[i];
+
             currGas += (gas[i] - cost[i]);
             if(currGas < 0){
                 start = i+1;
@@ -26,6 +17,6 @@ public:
             }
         }
 
-        return start;
+        return totGas < totCost ? -1 : start;
     }
 };
